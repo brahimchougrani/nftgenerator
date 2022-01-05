061a5f7ab16a88cf74fd793fb1eb0d0e644228d2
brahimchougrani <brahimchougrani31@gmail.com>
2022-01-05 21:43:55 +0100

    add domain

--- config/settings/production.py
+++ config/settings/production.py
@@ -6,7 +6,7 @@ from .base import env
 # https://docs.djangoproject.com/en/dev/ref/settings/#secret-key
 SECRET_KEY = env("DJANGO_SECRET_KEY")
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = env.list("DJANGO_ALLOWED_HOSTS", default=["ntf-cryptopunks-generator.com"])
+ALLOWED_HOSTS = env.list("DJANGO_ALLOWED_HOSTS", default=["gen.fridoc.com"])
 
 # DATABASES
 # ------------------------------------------------------------------------------
@@ -104,7 +104,7 @@ TEMPLATES[-1]["OPTIONS"]["loaders"] = [  # type: ignore[index] # noqa F405
 # ------------------------------------------------------------------------------
 # https://docs.djangoproject.com/en/dev/ref/settings/#default-from-email
 DEFAULT_FROM_EMAIL = env(
-    "DJANGO_DEFAULT_FROM_EMAIL", default="nft cryptopunks generator <noreply@ntf-cryptopunks-generator.com>"
+    "DJANGO_DEFAULT_FROM_EMAIL", default="nft cryptopunks generator <noreply@fridoc.com>"
 )
 # https://docs.djangoproject.com/en/dev/ref/settings/#server-email
 SERVER_EMAIL = env("DJANGO_SERVER_EMAIL", default=DEFAULT_FROM_EMAIL)
